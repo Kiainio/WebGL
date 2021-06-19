b66b4d8a6922c130d9f10b9e1c0d07b387549901
唐茵 <18373394@buaa.edu.cn>
2021-06-19 14:34:23 +0800

    first commit

---
+++ Hello world/how-it-works.js
@@ -0,0 +1,259 @@
+"use strict";
+
+function main() {
+    // Get A WebGL context
+    /** @type {HTMLCanvasElement} */
+    var canvas = document.querySelector("#canvas");
+    var gl = canvas.getContext("webgl");
+    if (!gl) {
+        return;
+    }
+
+    // setup GLSL program
+    var program = webglUtils.createProgramFromScripts(gl, ["vertex-shader-2d", "fragment-shader-2d"]);
+
+    // look up where the vertex data needs to go.
+    var positionLocation = gl.getAttribLocation(program, "a_position");
+    var resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+    var colorLocation = gl.getAttribLocation(program, "a_color");
+    var matrixLocation = gl.getUniformLocation(program, "u_matrix");
+
+    // Create a buffer to put positions in
+    var positionBuffer = gl.createBuffer();
+
+    // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    // 将几何数据存到缓冲
+    setGeometry(gl);
+
+    // 给颜色数据创建一个缓冲
+    var colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    // 设置颜色
+    setColors(gl);
+
+    var translation = [100, 150];
+    var angleInRadians = 0;
+    var scale = [1, 1];
+
+    drawScene();
+
+    // Setup a ui.
+    webglLessonsUI.setupSlider("#x", { value: translation[0], slide: updatePosition(0), max: gl.canvas.width });
+    webglLessonsUI.setupSlider("#y", { value: translation[1], slide: updatePosition(1), max: gl.canvas.height });
+    webglLessonsUI.setupSlider("#angle", { slide: updateAngle, max: 360 });
+    webglLessonsUI.setupSlider("#scaleX", { value: scale[0], slide: updateScale(0), min: -5, max: 5, step: 0.01, precision: 2 });
+    webglLessonsUI.setupSlider("#scaleY", { value: scale[1], slide: updateScale(1), min: -5, max: 5, step: 0.01, precision: 2 });
+
+    function updatePosition(index) {
+        return function (event, ui) {
+            translation[index] = ui.value;
+            drawScene();
+        };
+    }
+
+    function updateAngle(event, ui) {
+        var angleInDegrees = 360 - ui.value;
+        angleInRadians = angleInDegrees * Math.PI / 180;
+        drawScene();
+    }
+
+    function updateScale(index) {
+        return function (event, ui) {
+            scale[index] = ui.value;
+            drawScene();
+        };
+    }
+
+    // 绘制场景
+    function drawScene() {
+        webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+        // 告诉WebGL如何从裁剪空间对应到像素
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+        // 清空画布
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        // 使用我们的程序
+        gl.useProgram(program);
+
+        // 启用属性
+        gl.enableVertexAttribArray(positionLocation);
+
+        // 绑定位置缓冲
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+
+        // 告诉属性怎么从positionBuffer中读取数据 (ARRAY_BUFFER)
+        var size = 2;          // 每次迭代运行提取两个单位数据
+        var type = gl.FLOAT;   // 每个单位的数据类型是32位浮点型
+        var normalize = false; // 不需要归一化数据
+        var stride = 0;        // 0 = 移动单位数量 * 每个单位占用内存（sizeof(type)）
+        var offset = 0;        // 从缓冲起始位置开始读取
+        gl.vertexAttribPointer(
+            positionLocation, size, type, normalize, stride, offset)
+
+        gl.enableVertexAttribArray(colorLocation);
+
+        // 绑定颜色缓冲
+        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+
+        // 告诉颜色属性怎么从 colorBuffer (ARRAY_BUFFER) 中读取颜色值
+        var size = 4;          // 每次迭代使用4个单位的数据
+        var type = gl.FLOAT;   // 单位数据类型是32位的浮点型
+        var normalize = false; // 不需要归一化数据
+        var stride = 0;        // 0 = 移动距离 * 单位距离长度sizeof(type) 
+        // 每次迭代跳多少距离到下一个数据
+        var offset = 0;        // 从绑定缓冲的起始处开始
+        gl.vertexAttribPointer(
+            colorLocation, size, type, normalize, stride, offset)
+
+        // 计算矩阵
+        var matrix = m3.projection(gl.canvas.clientWidth, gl.canvas.clientHeight);
+        matrix = m3.translate(matrix, translation[0], translation[1]);
+        matrix = m3.rotate(matrix, angleInRadians);
+        matrix = m3.scale(matrix, scale[0], scale[1]);
+
+        // 设置矩阵
+        gl.uniformMatrix3fv(matrixLocation, false, matrix);
+
+        // 绘制几何体
+        var primitiveType = gl.TRIANGLES;
+        var offset = 0;
+        var count = 6;
+        gl.drawArrays(primitiveType, offset, count);
+    }
+}
+
+var m3 = {
+    translation: function (tx, ty) {
+        return [
+            1, 0, 0,
+            0, 1, 0,
+            tx, ty, 1,
+        ];
+    },
+
+    rotation: function (angleInRadians) {
+        var c = Math.cos(angleInRadians);
+        var s = Math.sin(angleInRadians);
+        return [
+            c, -s, 0,
+            s, c, 0,
+            0, 0, 1,
+        ];
+    },
+
+    scaling: function (sx, sy) {
+        return [
+            sx, 0, 0,
+            0, sy, 0,
+            0, 0, 1,
+        ];
+    },
+
+    multiply: function (a, b) {
+        var a00 = a[0 * 3 + 0];
+        var a01 = a[0 * 3 + 1];
+        var a02 = a[0 * 3 + 2];
+        var a10 = a[1 * 3 + 0];
+        var a11 = a[1 * 3 + 1];
+        var a12 = a[1 * 3 + 2];
+        var a20 = a[2 * 3 + 0];
+        var a21 = a[2 * 3 + 1];
+        var a22 = a[2 * 3 + 2];
+        var b00 = b[0 * 3 + 0];
+        var b01 = b[0 * 3 + 1];
+        var b02 = b[0 * 3 + 2];
+        var b10 = b[1 * 3 + 0];
+        var b11 = b[1 * 3 + 1];
+        var b12 = b[1 * 3 + 2];
+        var b20 = b[2 * 3 + 0];
+        var b21 = b[2 * 3 + 1];
+        var b22 = b[2 * 3 + 2];
+        return [
+            b00 * a00 + b01 * a10 + b02 * a20,
+            b00 * a01 + b01 * a11 + b02 * a21,
+            b00 * a02 + b01 * a12 + b02 * a22,
+            b10 * a00 + b11 * a10 + b12 * a20,
+            b10 * a01 + b11 * a11 + b12 * a21,
+            b10 * a02 + b11 * a12 + b12 * a22,
+            b20 * a00 + b21 * a10 + b22 * a20,
+            b20 * a01 + b21 * a11 + b22 * a21,
+            b20 * a02 + b21 * a12 + b22 * a22,
+        ];
+    },
+
+    identity: function () {
+        return [
+            1, 0, 0,
+            0, 1, 0,
+            0, 0, 1,
+        ];
+    },
+
+    projection: function (width, height) {
+        // 注意：这个矩阵翻转了 Y 轴，所以 0 在上方
+        return [
+            2 / width, 0, 0,
+            0, -2 / height, 0,
+            -1, 1, 1
+        ];
+    },
+
+
+    translate: function (m, tx, ty) {
+        return m3.multiply(m, m3.translation(tx, ty));
+    },
+
+    rotate: function (m, angleInRadians) {
+        return m3.multiply(m, m3.rotation(angleInRadians));
+    },
+
+    scale: function (m, sx, sy) {
+        return m3.multiply(m, m3.scaling(sx, sy));
+    },
+
+};
+
+// 在缓冲存储构成 'F' 的值
+function setGeometry(gl) {
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([
+            -150, -100,
+            150, -100,
+            -150, 100,
+            150, -100,
+            -150, 100,
+            150, 100]),
+        gl.STATIC_DRAW);
+}
+
+main();
+
+// 给矩形的两个三角形
+// 设置颜色值并发到缓冲
+function setColors(gl) {
+    // 生成两个随机颜色
+    var r1 = Math.random();
+    var b1 = Math.random();
+    var g1 = Math.random();
+
+    var r2 = Math.random();
+    var b2 = Math.random();
+    var g2 = Math.random();
+
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array(
+            [Math.random(), Math.random(), Math.random(), 1,
+            Math.random(), Math.random(), Math.random(), 1,
+            Math.random(), Math.random(), Math.random(), 1,
+            Math.random(), Math.random(), Math.random(), 1,
+            Math.random(), Math.random(), Math.random(), 1,
+            Math.random(), Math.random(), Math.random(), 1]),
+        gl.STATIC_DRAW);
+}
